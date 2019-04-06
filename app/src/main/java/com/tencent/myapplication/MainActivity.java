62092757ba7ef612c61eab8e5da251fa0b87a29a
hanszhli <lzh_hans@outlook.com>
2019-04-06 10:51:52 +0800

    no message

--- app/src/main/java/com/tencent/myapplication/MainActivity.java
+++ app/src/main/java/com/tencent/myapplication/MainActivity.java
@@ -69,11 +69,7 @@ public class MainActivity extends AppCompatActivity
         
         // master 2
 
-<<<<<<< .merge_file_ipEgB1
-        
-=======
         // v1.4-hotfix
->>>>>>> .merge_file_Ylawdi
     }
 
     @Override
